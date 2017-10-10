876e641c8460a7b3d31d5cadb6219413a379c74a
Bram Joosten <post@bramjoosten.nl>
2017-10-10 23:36:15 +0200

    anndd some more changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 // This is the Main.js file. It should be the final file in the build chain
+
+$("#scrolldown").click(function() {
+    $('html, body').animate({
+        scrollTop: $("main").offset().top
+    }, 500, "swing");
+});
